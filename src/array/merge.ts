a025305f6c127ac8d35e4a2726c2ba0ad97aee8e
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-08-20 12:06:10 -0700

    rewrite array merge utility function

--- src/array/merge.ts
+++ src/array/merge.ts
@@ -1,9 +1,7 @@
-import { accumulate } from './accumulate'
-
 export function merge<R, U, T>(arrayOfU: U[], arrayOfT: T[],  callback: (u: U | undefined, t:T | undefined) => R) {
   const mergedArrayLength = Math.max(arrayOfU.length, arrayOfT.length)
-  return accumulate((index, acc) => {
-    const mergedValue = callback(arrayOfU[index], arrayOfT[index])
-    return [ ...acc, mergedValue ]
-  }, mergedArrayLength, [] as R[])
+
+  return Array(mergedArrayLength).fill(undefined).map((_, index) => {
+    return callback(arrayOfU[index], arrayOfT[index])
+  })
 }
